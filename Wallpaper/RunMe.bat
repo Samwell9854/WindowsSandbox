0a9eb430a2ce9008fa4136297876f59a7c997e1d
Samuel Giroux <samuel.giroux@hotmail.com>
2019-08-02 16:30:43 -0400

    Added wallpaper script
    Adds PC name with domain name on default Windows background.
    Known bug: Will add trailing "." if PC not in a domain.

---
+++ Wallpaper/RunMe.bat
@@ -0,0 +1,4 @@
+%~dp0ImageMagick\convert.exe C:\Windows\Web\Wallpaper\Windows\img0.jpg -fill white -undercolor opaque -gravity SouthEast -pointsize 60 -font Calibri -annotate +50+50 %COMPUTERNAME%.%USERDNSDOMAIN% C:\Windows\Web\Wallpaper\Windows\img1.jpg
+regedit /s %~dp0Wallpaper.reg
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+pause
